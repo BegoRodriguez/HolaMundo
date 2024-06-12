d88c1dc1bdd9b1daeccdb490af7b800e761124d6
BegoRodriguez <136585766+BegoRodriguez@users.noreply.github.com>
2024-06-12 17:23:09 +0200

    Añadir algunas de las cosas básicas hechas con JS en el curso de Cisco

--- 03_arrays.js
+++ 03_arrays.js
@@ -9,3 +9,14 @@ console.log(dias[5]); // -> Sabado
 dias[0] = "Sunday";   // Podemos modificar el elemento accediento a través del indice
 console.log(dias[0]); // -> Sunday
    
+// Ejemplo length
+let nombres = ["Olivia", "Emma", "Mateo", "Samuel"];
+console.log(nombres.length); // -> 4
+   
+nombres[5] = "Amelia";
+console.log(nombres.length); // -> 6
+   
+console.log(nombres); // -> ["Olivia", "Emma", "Mateo","Samuel", undefined, "Amelia"]
+console.log(nombres[3]); // -> Samuel
+console.log(nombres[4]); // -> undefined
+console.log(nombres[5]); // -> Amelia
--- 04_ejercicios.js
+++ 04_ejercicios.js
@@ -29,4 +29,53 @@ person.name = "Paola"
 person.surname = "Pérez"
 console.log(`Name: ${person.name} Surname: ${person.surname} `)
 
-//Para ver si me deja subir
+/* Question 3: We are setting up our small library of books 
+about JavaScript programming. We have three books and want 
+to prepare a list of them. We will store three pieces information 
+about each book: title, author, and number of pages:
+
+Speaking JavaScript, Axel Rauschmayer, 460;
+Programming JavaScript Applications, Eric Elliott, 254;
+Understanding ECMAScript 6, Nicholas C. Zakas, 352.
+Create an array of three objects representing the books. 
+Each object must have the following properties: title, author, pages.*/
+
+let books = [{title:"Speaking JavaScript",author:"Axel Rauschmayer",pages:460},
+    {title:"Programming JavaScript Applications",author:"Eric Elliott",pages:254},
+    {title:"Understanding ECMAScript 6",author:"Nicholas C. Zakas",pages:352}
+]
+
+/* Question 4: Add a new book to the collection: 
+    Learning JavaScript Design Patterns, by Addy Osmani, 254 pages. 
+    Use the appropriate method to do this, which will attach the book at the end 
+    of the array. Display the length of the array and, in turn, all the book names 
+    in the collection.*/
+books.push({title:"Learning JavaScript Design Patterns",author:"Addy Osmani",pages:254})
+console.log(`Longitud del array: ${books.length}`)
+
+for (value in books){
+    console.log(books[value].title)
+}
+
+/* Question 5: Use the slice command to copy the last two books to the new array.*/
+let lastTwoBooks = books.slice(-2)
+console.log(lastTwoBooks)
+
+/* Question 6: The first book from the collection is lost 
+in unexplained circumstances. You have already accepted the loss, 
+so remove it from the array. Which method will you use for this purpose? 
+Display the length of the array and all the names of the books from the 
+collection in turn. */
+books.shift()
+console.log(books.length)
+for (value in books){
+    console.log(books[value].title)
+}
+
+/*Question 7: Display the sum of the pages of all the books from the collection.*/
+let total = 0
+for (value in books){
+    total += books[value].pages
+}
+
+console.log(`El total de páginas es ${total} `)
